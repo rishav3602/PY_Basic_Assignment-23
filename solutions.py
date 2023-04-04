3ed16daa73290f2fc1a3c0bcd8d5d646efc0fb8d
Rishav Kumar <113835701+rishav3602@users.noreply.github.com>
2023-04-04 15:09:38 +0530

    Update solutions.py

--- solutions.py
+++ solutions.py
@@ -6,7 +6,9 @@ print(a, b, c)
 
 ANSWER...
 
-The result of the code will be: 1 2 8. Here, the function func has 3 parameters: a, b, and c, with default values for b and c. When the function is called with func(1, 2), the value of a is set to 1, the value of b is set to 2, and the default value of c (which is 8) is used.
+The result of the code will be: 1 2 8. Here, the function func has 3 parameters: a, b, and c,
+with default values for b and c. When the function is called with func(1, 2), the value of a
+is set to 1, the value of b is set to 2, and the default value of c (which is 8) is used.
 
 ------------------------------------------------------------------------------------------------
 
@@ -18,7 +20,10 @@ print(a, b, c)
 
 ANSWER...
 
-The result of the code will be: 1 2 3. Here, the function func has 3 parameters: a, b, and c, with a default value for c. When the function is called with func(1, c=3, b=2), the value of a is set to 1, the value of b is set to 2, and the value of c is set to 3, overriding the default value of c.
+The result of the code will be: 1 2 3. Here, the function func has 3 parameters: a, b, and c,
+with a default value for c. When the function is called with func(1, c=3, b=2), the value of
+a is set to 1, the value of b is set to 2, and the value of c is set to 3, overriding the
+default value of c.
 
 ------------------------------------------------------------------------------------------------
 
@@ -30,7 +35,9 @@ print(a, pargs)
 
 ANSWER...
 
-The result of the code will be: 1 (2, 3). Here, the function func has 1 required parameter (a) and a variable-length argument list (*pargs). When the function is called with func(1, 2, 3), the value of a is set to 1, and the remaining arguments (2 and 3) are collected into a tuple pargs.
+The result of the code will be: 1 (2, 3). Here, the function func has 1 required parameter
+(a) and a variable-length argument list (*pargs). When the function is called with func(1, 2, 3),
+the value of a is set to 1, and the remaining arguments (2 and 3) are collected into a tuple pargs.
 
 
 ------------------------------------------------------------------------------------------------
@@ -43,7 +50,10 @@ print(a, kargs)
 
 ANSWER...
 
-The result of the code will be: 1 {'c': 3, 'b': 2}. Here, the function func has 1 required parameter (a) and a variable-length keyword argument list (**kargs). When the function is called with func(a=1, c=3, b=2), the value of a is set to 1, and the remaining keyword arguments (c=3 and b=2) are collected into a dictionary kargs.
+The result of the code will be: 1 {'c': 3, 'b': 2}. Here, the function func has 1 required
+parameter (a) and a variable-length keyword argument list (**kargs). When the function is
+called with func(a=1, c=3, b=2), the value of a is set to 1, and the remaining keyword
+arguments (c=3 and b=2) are collected into a dictionary kargs.
 
 
 ------------------------------------------------------------------------------------------------
@@ -55,7 +65,10 @@ The result of the code will be: 1 {'c': 3, 'b': 2}. Here, the function func has 
 
 ANSWER...
 
-The result of the code will be: 1 5 6 5. Here, the function func has 3 parameters: a, b, and c, with default values for c and d. When the function is called with func(1, *(5, 6)), the value of a is set to 1, the value of b is unpacked from (5, 6) and set to 5, and the default value of c (which is 8) is used, and the default value of d (which is 5) is used.
+The result of the code will be: 1 5 6 5. Here, the function func has 3 parameters: a, b, and c,
+with default values for c and d. When the function is called with func(1, *(5, 6)), the value
+of a is set to 1, the value of b is unpacked from (5, 6) and set to 5, and the default value
+of c (which is 8) is used, and the default value of d (which is 5) is used.
 
 
 ------------------------------------------------------------------------------------------------
@@ -70,7 +83,11 @@ The result of the code will be: 1 5 6 5. Here, the function func has 3 parameter
 
 ANSWER...
 
-The result of the code will be: 1, ['x'], {'a': 'y'}. Here, the function func has 3 parameters: a, b, and c. When the function is called with func(l, m, n), the value of l is set to 1, the value of m is passed by reference and changed to ['x'], and the value of n is passed by reference and changed to {'a': 'y'}. After the function call, the value of l remains 1, the value of m is ['x'], and the value of n is {'a': 'y'}.
+The result of the code will be: 1, ['x'], {'a': 'y'}. Here, the function func has 3 parameters:
+a, b, and c. When the function is called with func(l, m, n), the value of l is set to 1, the
+value of m is passed by reference and changed to ['x'], and the value of n is passed by reference
+and changed to {'a': 'y'}. After the function call, the value of l remains 1, the value of m is
+['x'], and the value of n is {'a': 'y'}.
 
 
 ------------------------------------------------------------------------------------------------
